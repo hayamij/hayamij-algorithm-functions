7b1cefaee5b1bd0a9bb7684a6841f66c27134193
Nguyen Quang Tuan Phuong <130271080+hayamij@users.noreply.github.com>
2024-07-07 19:02:39 +0700

    Update sentinalLinearSearch.cpp

--- search functions/sentinalLinearSearch.cpp
+++ search functions/sentinalLinearSearch.cpp
@@ -3,10 +3,12 @@ int sentinalLinearSearch(int a[], int n, int x);
 
 
 // functions
-int sentinalLinearSearch(int a[], int n, int x) { 
-    a[n] = x;  
-    for (int i = 0; ;i++){ 
-        if (a[i] == x) return i;
+int sentinalLinearSearch(int a[], int n, int x){
+    a[n] = x; //đặt lính canh 
+    for (int i = 0; ; i++){
+        if (a[i]==x){
+            if (i == n) return -1;
+            return i;
+        }
     }
-    return -1;
 }
