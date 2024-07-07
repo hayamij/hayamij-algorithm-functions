d612ac7ccb1e6926b88227153ffba72159a1fa78
Nguyen Quang Tuan Phuong <130271080+hayamij@users.noreply.github.com>
2024-07-07 18:33:09 +0700

    Update binarySearch.cpp

--- search functions/binarySearch.cpp
+++ search functions/binarySearch.cpp
@@ -4,12 +4,13 @@ int binarySearch(int arr[], int l, int r, int x);
 
 // functions
 int binarySearch(int arr[], int l, int r, int x) { 
-    if (r >= l) { 
+    while (r >= l) { 
         int mid = l + (r - l) / 2; 
         if (arr[mid] == x) 
             return mid; 
         if (arr[mid] > x) 
-        return binarySearch(arr, mid + 1, r, x); 
+            return binarySearch(arr, l, mid - 1, x); 
+        return binarySearch(arr, mid + 1, r, x);
     } 
     return -1; 
 } 
